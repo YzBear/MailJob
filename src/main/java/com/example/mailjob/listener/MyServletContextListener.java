20b4d082138144385e6affd2e1f08fe3a4ad1890
yzx <iyanzixiong@163.com>
2021-06-17 18:35:35 +0800

    增加user.txt的文件读取和生日日期过滤

--- src/main/java/com/example/mailjob/listener/MyServletContextListener.java
+++ src/main/java/com/example/mailjob/listener/MyServletContextListener.java
@@ -1,8 +1,12 @@
 package com.example.mailjob.listener;
 import com.example.mailjob.entity.User;
 import com.example.mailjob.util.MailUtils;
+
+import java.io.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -21,8 +25,8 @@ public class MyServletContextListener implements ServletContextListener {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                List<User> userList = new ArrayList<>();
-                userList.add(new User("123","李四",new Date(),"iyanzixiong@163.com"));
+                List<User> userList;
+               userList =  getUsers();
                 if (userList!=null ) {
                     for (User user : userList) {
                         String emailMsg="亲爱的"+user.getName()+"<br/>祝你生日快乐";
@@ -36,6 +40,28 @@ public class MyServletContextListener implements ServletContextListener {
     public void contextDestroyed(ServletContextEvent arg0)  {
         // TODO Auto-generated method stub
     }
+    public List<User> getUsers() {
+        List<User> temp= new ArrayList<>();
+        File file =new File("src/main/resources/User.txt");
+        FileInputStream in;
+        try {
+            in = new FileInputStream(file);
+            InputStreamReader objIn=new InputStreamReader (in);
+            BufferedReader bfr=new BufferedReader(objIn);
+            String line;
+            while((line = bfr.readLine())!=null)
+            {
+                String[] split = line.split(",");
+                Date date = sdf.parse(split[2]);
+                User tempUser = new User(split[0],split[1],date,split[3]);
+                temp.add(tempUser);
+            }
+            objIn.close();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
 
+        return  temp.stream().filter(s->!sdf.format(s.getBirthday()).equals(sdf.format(new Date()))).collect(Collectors.toList());
+    }
 
 }
